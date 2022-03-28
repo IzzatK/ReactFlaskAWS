e4ef59b521822b558182f4b89f370f8e6e55651d
IzzatK <Izzatkhadim@gmail.com>
2022-03-27 20:03:02 -0400

    onto Flask JWT Auth

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+import os
+from decouple import config 
+
+
+
+class Config:
+    SECRET_KEY=config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI="postgresql://postgres@localhost/recipeapp"
+    DEBUG=True
+    SQLALCHEMY_ECHO=True
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
